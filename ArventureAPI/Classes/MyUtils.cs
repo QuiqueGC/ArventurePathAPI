2035a76a2b8486e18fbef5a2cf3428d7b7552287
Quique <eguevarac2223@politecnics.barcelona>
2024-03-05 20:04:13 +0100

    añadida la clase Utils con la devolución de errores

---
+++ ArventureAPI/Classes/MyUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ArventureAPI.Classes
+{
+    public static class MyUtils
+    {
+        /// <summary>
+        /// Extrae el mensaje de error de la excepción pertinente
+        /// </summary>
+        /// <param name="ex">la excepción que ha ocurrido</param>
+        /// <returns>el mensaje de la excepción</returns>
+        public static String ErrorMessage(SqlException ex)
+        {
+            String msg = "";
+
+            switch (ex.Number)
+            {
+                case 2:
+                    msg = "Servidor no operativo";
+                    break;
+                case 547:
+                    msg = "El registro tiene datos relacionados";
+                    break;
+                case 2601:
+                    msg = "Registro duplicado";
+                    break;
+                case 4060:
+                    msg = "No se puede abrir la base de datos";
+                    break;
+                case 18456:
+                    msg = "Error al iniciar sesión";
+                    break;
+                default:
+                    msg = ex.Number + " : " + ex.Message;
+                    break;
+            }
+            return msg;
+        }
+    }
+}
---
+++ ArventureAPI/Controllers/arventuresController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ArventureAPI.Models;
+
+namespace ArventureAPI.Controllers
+{
+    public class arventuresController : ApiController
+    {
+        private abp__politecnics_com_dam01Entities db = new abp__politecnics_com_dam01Entities();
+
+        // GET: api/arventures
+        public IQueryable<arventure> Getarventure()
+        {
+            //Evita que cargue los datos relacionados (y así cargar sólo las areventures per se)
+            db.Configuration.LazyLoadingEnabled = false;
+            return db.arventure;
+        }
+
+        // GET: api/arventures/5
+        [ResponseType(typeof(arventure))]
+        public async Task<IHttpActionResult> Getarventure(int id)
+        {
+            IHttpActionResult result;
+            //Evita que cargue los datos relacionados (y así cargar sólo las arventures per se)
+            db.Configuration.LazyLoadingEnabled = false;
+
+            //cicles cicles = await db.cicles.FindAsync(id);
+
+            //Queremos que nos devuelva ciclos con curso
+            arventure _arventure = await db.arventure
+                .Include("route")
+                .Where(c => c.id == id)
+                .FirstOrDefaultAsync();
+
+            if (_arventure == null)
+            {
+                result = NotFound();
+            }
+            else
+            {
+                result = Ok(_arventure);
+            }
+
+            return result;
+
+
+
+
+
+
+
+
+            /*
+            arventure arventure = await db.arventure.FindAsync(id);
+            if (arventure == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(arventure);
+            */
+        }
+
+        // PUT: api/arventures/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putarventure(int id, arventure arventure)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != arventure.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(arventure).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!arventureExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/arventures
+        [ResponseType(typeof(arventure))]
+        public async Task<IHttpActionResult> Postarventure(arventure arventure)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.arventure.Add(arventure);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = arventure.id }, arventure);
+        }
+
+        // DELETE: api/arventures/5
+        [ResponseType(typeof(arventure))]
+        public async Task<IHttpActionResult> Deletearventure(int id)
+        {
+            arventure arventure = await db.arventure.FindAsync(id);
+            if (arventure == null)
+            {
+                return NotFound();
+            }
+
+            db.arventure.Remove(arventure);
+            await db.SaveChangesAsync();
+
+            return Ok(arventure);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool arventureExists(int id)
+        {
+            return db.arventure.Count(e => e.id == id) > 0;
+        }
+    }
+}
